ebe24f940042b91f7c8e7d2394aebde17b9f597f
Michal Miszczyszyn <michal.miszczyszyn@gmail.com>
2012-07-16 08:10:17 -0400

    Add self executing function to create private scope and to prevent jQuery conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
-var Website = {
-  
-  run: function(){
-    this.foo();
-  },
-  
-  foo: function(){
-    // your code
-  }
-  
-}
-
-$(function(){
-  
-  Website.run();
-  
-});
+/**
+ * Self executing function taking jQuery as a parameter.
+ * Creates private-scope. Also if jQuery is run in noConflict() mode
+ * the $ sign can be used inside this function.
+ */
+(function ($) {
+    var Website = {
+      
+      run: function(){
+        this.foo();
+      },
+      
+      foo: function(){
+        // your code
+      }
+      
+    };
+    
+    $(function(){
+      
+      Website.run();
+      
+      /**
+       * Optional: export Website to the global namespace so that it's accessible
+       * from other parts of the code
+       */
+      // window.Website = window.Website || Website;
+      
+    });
+})(jQuery);
